8de8531d59ba2e488cbffaf7e27ab13ef5937139
DennisWG <DennisWG@users.noreply.github.com>
2016-12-27 19:02:32 +0100

    copyright notice

--- src/lib/dummy.cpp
+++ src/lib/dummy.cpp
@@ -14,23 +14,3 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 
-#define TESTINATOR_MAIN
-#include <testinator.h>
-
-DEF_TEST(Name, Suite)
-{
-    return true;
-}
-
-DEF_TIMED_TEST(Name2, Suite)
-{
-}
-
-DEF_PROPERTY(Name3, Suite, int)
-{
-    return true;
-}
-
-DEF_COMPLEXITY_PROPERTY(Name4, Suite, ORDER_N, int)
-{
-}
