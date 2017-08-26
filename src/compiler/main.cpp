a563ffcbf519ba5b8e2bf2287befbe8ee2e23565
DennisWG <DennisWG@users.noreply.github.com>
2017-08-27 00:50:47 +0200

    improved upon the compiler

--- src/compiler/main.cpp
+++ src/compiler/main.cpp
@@ -4,48 +4,50 @@
 
 #include <flatmessage/compiler.hpp>
 
-cxxopts::Options ParseCommandLine(int argc, char** argv)
+int main(int argc, char** argv)
 {
-    cxxopts::Options options("flatmessage_compiler", "A compiler for flatmessages");
+    cxxopts::Options options("flatmessage_compiler", "flatmessage compiler");
 
     // clang-format off
     options.add_options()
         ("e,extension", "The extension used for the output file", cxxopts::value<std::string>())
-        ("i,input", "The input file", cxxopts::value<std::vector<std::string>>())
+        ("i,inputs", "The input file(s)", cxxopts::value<std::vector<std::string>>())
         ("t,template", "The template file", cxxopts::value<std::string>())
-        ("w,workdir", "This is the root directory of all your flatmessage files", cxxopts::value<std::string>())
+        ("o,outputDirectory", "The directory where all of your compiled flatmessage files will be written to", cxxopts::value<std::string>())
         ;
     // clang-format on
 
+    if (argc == 1 || (argc == 2 && (argv[1] == std::string{"--help"} || argv[1] == std::string{"-h"})))
+    {
+        std::cout << options.help();
+        return 0;
+    }
+
     options.parse_positional("input");
-
-    options.parse(argc, argv);
-
-    return options;
-}
-
-int main(int argc, char** argv)
-{
-    auto options = ParseCommandLine(argc, argv);
 
     try
     {
+        options.parse(argc, argv);
+
         auto extension = options["e"].as<std::string>();
         auto inputs = options["i"].as<std::vector<std::string>>();
         auto tmplate = options["t"].as<std::string>();
-        auto workdir = options["w"].as<std::string>();
+        auto outDir = options["o"].as<std::string>();
+
+        if (extension.empty() || inputs.empty() || tmplate.empty() || outDir.empty())
+            return -1;
 
         std::vector<flatmessage::file_template_pair> files;
         for (auto&& input : inputs)
             files.emplace_back(flatmessage::file_template_pair{input, tmplate});
 
         flatmessage::compiler compiler;
-        compiler.compile_files(files, {1, workdir, extension});
+        compiler.compile_files(files, {1, outDir, extension});
     }
     catch (std::exception& e)
     {
         std::cout << e.what() << '\n';
-        return -1;
+        return -2;
     }
 
     return 0;
