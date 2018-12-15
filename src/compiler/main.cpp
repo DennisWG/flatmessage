1979d4459305eb1771a05a72c96a4d8bc0736f67
DennisWG <DennisWG@users.noreply.github.com>
2018-12-15 16:08:18 +0100

    Added `--mergeOutputs` (`-m`) to the compiler's command line arguments. Merges all given input files into one output file

--- src/compiler/main.cpp
+++ src/compiler/main.cpp
@@ -14,6 +14,7 @@ int main(int argc, char** argv)
         ("i,inputs", "The input file(s)", cxxopts::value<std::vector<std::string>>())
         ("t,template", "The template file", cxxopts::value<std::string>())
         ("o,outputDirectory", "The directory where all of your compiled flatmessage files will be written to", cxxopts::value<std::string>())
+        ("m,mergeOutputs", "Merges all outputs into one big file", cxxopts::value<bool>()->default_value("false"))
         ;
     // clang-format on
 
@@ -33,6 +34,7 @@ int main(int argc, char** argv)
         auto inputs = result["i"].as<std::vector<std::string>>();
         auto tmplate = result["t"].as<std::string>();
         auto outDir = result["o"].as<std::string>();
+        auto merge = result["m"].as<bool>();
 
         if (extension.empty() || inputs.empty() || tmplate.empty() || outDir.empty())
             return -1;
@@ -41,8 +43,9 @@ int main(int argc, char** argv)
         for (auto&& input : inputs)
             files.emplace_back(flatmessage::file_template_pair{input, tmplate});
 
+        auto flags = merge ? flatmessage::compiler_flags::merge_translation_units : flatmessage::compiler_flags::none;
         flatmessage::compiler compiler;
-        compiler.compile_files(files, {1, outDir, extension});
+        compiler.compile_files(files, {1, outDir, extension, flags});
     }
     catch (std::exception& e)
     {
